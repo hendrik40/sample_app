606f7d779323b2471727452fccf350d7d98e585e
hendrik susanto <hendrik.scheherazade@gmail.com>
2014-01-08 01:03:34 +0700

    tambah halaman contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,7 +27,18 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
+  describe "Contact page" do
 
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
   describe "About page" do
 
     it "should have the content 'About Us'" do
